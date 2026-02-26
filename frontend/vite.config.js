ddd7cadf73f3f8e7abdb13e451ae44af5fbf3b9d
mayconspirlandelli <mayconbatista8@gmail.com>
2026-02-26 15:12:43 -0300

    Ajustado as configurações de IP do Vite.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: "0.0.0.0",
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true
       }
     }
